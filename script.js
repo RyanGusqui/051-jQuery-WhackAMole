0f9ea1bd0f2020a9c7a15fd2737ef1968c3c1607
Gusqui <43656006+Ryanix24@users.noreply.github.com>
2019-04-09 17:08:20 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+$("#mole1").click(function(){
+    $("#mole1").hide();
+});
+    
+$("#moleTwo").click(function(){
+    $("#moleTwo").hide();
+}); 
+    
+
+    
+$("#button").click(function(){
+    $("#mole1").show();
+    $("#moleTwo").show();
+    $("#thirdMole").show();
+});
+$("#thirdMole").click(function(){
+    $("#mole1").show();
+    $("#moleTwo").show();
+});
